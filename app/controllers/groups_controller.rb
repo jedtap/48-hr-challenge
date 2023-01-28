7274b8e2e50d469391997da998b6094a9d662490
jedtap <doom.slaver2@gmail.com>
2023-01-28 15:16:14 +0800

    Creating new groups saves the userID who created it

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -65,6 +65,6 @@ class GroupsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def group_params
-      params.require(:group).permit(:name)
+      params.require(:group).permit(:name, :user_id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_28_041209) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_28_070646) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_28_041209) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
