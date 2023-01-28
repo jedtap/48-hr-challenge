b3bf6cdcb6bba63a57d28d41294358421104e9d8
jedtap <doom.slaver2@gmail.com>
2023-01-28 17:02:21 +0800

    created membership relationships

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,7 @@
 class Group < ApplicationRecord
     belongs_to :user
     has_many :posts
+
+    has_many :memberships
+    has_many :users, through: :memberships
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_28_070646) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_28_085244) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_28_070646) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_groups_on_user_id"
+  end
+
+  create_table "memberships", force: :cascade do |t|
+    t.integer "group_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_memberships_on_group_id"
+    t.index ["user_id"], name: "index_memberships_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
