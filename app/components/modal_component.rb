4a67feeb85f739e92f6aca923c46b3bee026fa5d
jedtap <doom.slaver2@gmail.com>
2023-01-28 21:03:48 +0800

    Gear icon opens modal to rename group

--- app/components/modal_component.rb
+++ app/components/modal_component.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class ModalComponent < ViewComponent::Base
-  def initialize(title, user_id)
+  def initialize(title, user_id, group)
     @title = title
     @user_id = user_id
+    @group = group
   end
 
 end
