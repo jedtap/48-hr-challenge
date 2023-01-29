cfcad68e75931ec742ba8e5953b4020205bf175e
jedtap <doom.slaver2@gmail.com>
2023-01-29 22:02:58 +0800

    edits for railway app deployment

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: bundle exec puma -C config/puma.rb
+web: rails db:create && rails db:migrate && rails s -b 0.0.0.0 -p $PORT
 release: bundle exec rake db:migrate
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.hosts << "rails-production-9264.up.railway.app"
+
   # Devise default url
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
