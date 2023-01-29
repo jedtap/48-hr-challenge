769d70d6c0f4ee9281c39ba5fc961125b514db10
jedtap <doom.slaver2@gmail.com>
2023-01-29 16:30:10 +0800

    cosmetic changes, new field for modal component

--- app/components/modal_component.rb
+++ app/components/modal_component.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class ModalComponent < ViewComponent::Base
-  def initialize(title, user_id, group)
+  def initialize(title, user_id, group, label)
     @title = title
     @user_id = user_id
     @group = group
+    @label = label
   end
 
 end
